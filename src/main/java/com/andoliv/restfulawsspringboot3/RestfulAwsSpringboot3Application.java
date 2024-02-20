d7bf3b27786dd5b785dd813dbd78a70855cb7f4f
Anderson de Oliveira <andoliv@gmail.com>
2024-02-20 13:31:12 +1300

    Initial commit.

---
+++ src/main/java/com/andoliv/restfulawsspringboot3/RestfulAwsSpringboot3Application.java
@@ -0,0 +1,13 @@
+package com.andoliv.restfulawsspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulAwsSpringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulAwsSpringboot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/andoliv/restfulawsspringboot3/RestfulAwsSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andoliv.restfulawsspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulAwsSpringboot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
